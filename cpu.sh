1302dd2264116b2f0082e205b3290355fb9f44b8
vedhagsvp <vedhag230223@gmail.com>
2025-03-18 14:29:17 -0700

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./miner-linux-amd64 -targetMiner 0x1932E17CB48175Fd79FD08596eCd246071913Cb4 -domain https://sgp-mining.x-phere.com,https://bkk-mining.x-phere.com,https://hkg-mining.x-phere.com,https://idn-mining.x-phere.com
